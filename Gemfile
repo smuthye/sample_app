a360465c879373bd145e7920045d75e728b1006b
Surendra Muthye <surendra@comitydesigns.com>
2014-08-03 22:31:50 -0700

    Make a basic User model including secure passwords

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.2'
 gem 'rails', '4.1.4'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 
 gem 'pg', '0.17.1'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140804051347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,5 +75,15 @@ ActiveRecord::Schema.define(version: 0) do
 
   add_index "contact", ["lastmodifieddate"], name: "contact_idx_lastmodifieddate", using: :btree
   add_index "contact", ["sfid"], name: "contact_idx_sfid", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
